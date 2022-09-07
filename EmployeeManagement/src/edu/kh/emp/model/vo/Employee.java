af0a453f146d7fc9fd2f89d7507daa2e3a8f2186
baekSh1992 <p5tential@naver.com>
2022-09-07 17:48:22 +0900

    2022-09-07 사원관리 프로그램

--- EmployeeManagement/src/edu/kh/emp/model/vo/Employee.java
+++ EmployeeManagement/src/edu/kh/emp/model/vo/Employee.java
@@ -2,92 +2,168 @@ package edu.kh.emp.model.vo;
 
 // VO(Value Object) : 값 저장용 객체 (== DB 조회 결과 한 행을 저장)
 public class Employee {
-   private int empId; // 사원 번호(사번)
-   private String empName; // 사원 이름
-   private String empNo; // 주민등록번호
-   private String email; // 이메일
-   private String phone; // 전화번호
-   private String departmentTitle; // 부서명
-   private String jobName; // 직급명
-   private int salary; // 급여
+	private int empId; 		// 사원 번호(사번)
+	private String empName; // 사원 이름
+	private String empNo; 	// 주민등록번호
+	private String email; 	// 이메일
+	private String phone; 	// 전화번호
+	private String departmentTitle; // 부서명
+	private String jobName; // 직급명
+	private int salary; 	// 급여
+	
+	private String deptCode; // 부서코드
+	private String jobCode; // 직급코드
+	private String salLevel; // 급여등급
+	private double bonus; // 보너스
+	private int managerId; // 사수번호
+	
+	
+	
+	public Employee() {} // 기본 생성자
 
- //  alt + shift + o + enter 
-   public Employee(int empId, String empName, String empNo, String email, String phone, String departmentTitle,
-		String jobName, int salary) {
-	super();
-	this.empId = empId;
-	this.empName = empName;
-	this.empNo = empNo;
-	this.email = email;
-	this.phone = phone;
-	this.departmentTitle = departmentTitle;
-	this.jobName = jobName;
-	this.salary = salary;
-}
-// 게터/세터 alt + shift + s + 셀렉트 all + 제네레이트
-public int getEmpId() {
-	return empId;
-}
+	public Employee(int empId, String empName, String empNo, String email, String phone, String departmentTitle,
+			String jobName, int salary) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empNo = empNo;
+		this.email = email;
+		this.phone = phone;
+		this.departmentTitle = departmentTitle;
+		this.jobName = jobName;
+		this.salary = salary;
+	}
 
-public void setEmpId(int empId) {
-	this.empId = empId;
-}
+	// 매개변수 11개짜리 생성자
+	// 기본 생성자 단축키 alt + shift + s + o 
+	public Employee(int empId, String empName, String empNo, String email, String phone, int salary, String deptCode,
+			String jobCode, String salLevel, double bonus, int managerId) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empNo = empNo;
+		this.email = email;
+		this.phone = phone;
+		this.salary = salary;
+		this.deptCode = deptCode;
+		this.jobCode = jobCode;
+		this.salLevel = salLevel;
+		this.bonus = bonus;
+		this.managerId = managerId;
+	}
 
-public String getEmpName() {
-	return empName;
-}
+	public int getEmpId() {
+		return empId;
+	}
 
-public void setEmpName(String empName) {
-	this.empName = empName;
-}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
 
-public String getEmpNo() {
-	return empNo;
-}
+	public String getEmpName() {
+		return empName;
+	}
 
-public void setEmpNo(String empNo) {
-	this.empNo = empNo;
-}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
 
-public String getEmail() {
-	return email;
-}
+	public String getEmpNo() {
+		return empNo;
+	}
 
-public void setEmail(String email) {
-	this.email = email;
-}
+	public void setEmpNo(String empNo) {
+		this.empNo = empNo;
+	}
 
-public String getPhone() {
-	return phone;
-}
+	public String getEmail() {
+		return email;
+	}
 
-public void setPhone(String phone) {
-	this.phone = phone;
-}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-public String getDepartmentTitle() {
-	return departmentTitle;
-}
+	public String getPhone() {
+		return phone;
+	}
 
-public void setDepartmentTitle(String departmentTitle) {
-	this.departmentTitle = departmentTitle;
-}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 
-public String getJobName() {
-	return jobName;
-}
+	public String getDepartmentTitle() {
+		return departmentTitle;
+	}
 
-public void setJobName(String jobName) {
-	this.jobName = jobName;
-}
+	public void setDepartmentTitle(String departmentTitle) {
+		this.departmentTitle = departmentTitle;
+	}
 
-public int getSalary() {
-	return salary;
-}
+	public String getJobName() {
+		return jobName;
+	}
 
-public void setSalary(int salary) {
-	this.salary = salary;
-}
-   
-   
+	public void setJobName(String jobName) {
+		this.jobName = jobName;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getDeptCode() {
+		return deptCode;
+	}
+
+	public void setDeptCode(String deptCode) {
+		this.deptCode = deptCode;
+	}
+
+	public String getJobCode() {
+		return jobCode;
+	}
+
+	public void setJobCode(String jobCode) {
+		this.jobCode = jobCode;
+	}
+
+	public String getSalLevel() {
+		return salLevel;
+	}
+
+	public void setSalLevel(String salLevel) {
+		this.salLevel = salLevel;
+	}
+
+	public double getBonus() {
+		return bonus;
+	}
+
+	public void setBonus(double bonus) {
+		this.bonus = bonus;
+	}
+
+	public int getManagerId() {
+		return managerId;
+	}
+
+	public void setManagerId(int managerId) {
+		this.managerId = managerId;
+	}
+
+	// 알 + 슆 + s + s
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empNo=" + empNo + ", email=" + email
+				+ ", phone=" + phone + ", departmentTitle=" + departmentTitle + ", jobName=" + jobName + ", salary="
+				+ salary + ", deptCode=" + deptCode + ", jobCode=" + jobCode + ", salLevel=" + salLevel + ", bonus="
+				+ bonus + ", managerId=" + managerId + "]";
+	}
+
+	
 }
